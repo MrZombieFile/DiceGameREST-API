cdd51ea21b415c3e9de116d97e6383a8c52e79d7
MrZombieFile <afernahe@gmail.com>
2022-10-20 23:19:09 +0200

    tunning de responseEntities

--- src/main/java/com/example/JWT/api/AppController.java
+++ src/main/java/com/example/JWT/api/AppController.java
@@ -19,79 +19,129 @@ public class AppController {
     private final AppServiceImpl appService;
 
     @GetMapping("/players")
-    public ResponseEntity<Map<String, Float>> getResults(){
-        return ResponseEntity.ok().body(appService.getRankingUsers());
+    public ResponseEntity<?> getResults(){
+        try {
+            return ResponseEntity.ok().body(appService.getRankingUsers());
+        }catch(Exception exc){
+            return ResponseEntity.internalServerError().body("There was an error in the server");
+        }
     }
 
     @PostMapping("/players")
-    public ResponseEntity<AppUser> createUSer(@RequestBody AppUser appUser){
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/players").toUriString());
-        AppUser created = appService.addUser(appUser);
-        System.out.println("\n\n\n\n\n " + appUser.getUserName() + appUser.getPassword() + appUser.getName());
-        return ResponseEntity.created(uri).body(created);
+    public ResponseEntity<?> createUSer(@RequestBody AppUser appUser){
+        try {
+            URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/players").toUriString());
+            AppUser created = appService.addUser(appUser);
+            return ResponseEntity.created(uri).body(created);
+        }catch(Exception exc){
+            return ResponseEntity.internalServerError().body("There was an error in the server");
+        }
     }
 
     @PutMapping("/players")
-    public ResponseEntity<AppUser> modifyAppUser(@RequestBody AppUser appUser){
-        return ResponseEntity.accepted().body(appService.modifyUser(appUser));
+    public ResponseEntity<?> modifyAppUser(@RequestBody AppUser appUser){
+        try {
+            return ResponseEntity.accepted().body(appService.modifyUser(appUser));
+        }catch (Exception exc){
+            return ResponseEntity.internalServerError().body("There was an error in the server");
+        }
     }
 
     @GetMapping("/players/ranking")
-    public ResponseEntity<Double> getRankingTotalResum(){
-        return ResponseEntity.ok().body(appService.getResultatsTotals());
+    public ResponseEntity<?> getRankingTotalResum(){
+        try{
+            return ResponseEntity.ok().body(appService.getResultatsTotals());
+        }catch (Exception exc){
+            return ResponseEntity.internalServerError().body("There was an error in the server");
+        }
     }
 
     @GetMapping("/players/ranking/looser")
-    public ResponseEntity<AppUser> getLooser(){
-        return ResponseEntity.ok().body(appService.getLooser());
+    public ResponseEntity<?> getLooser(){
+        try{
+            return ResponseEntity.ok().body(appService.getLooser());
+        }catch (Exception exc){
+            return ResponseEntity.internalServerError().body("There was an error in the server");
+        }
     }
 
     @GetMapping("/players/ranking/winner")
-    public ResponseEntity<AppUser> getWinner(){
-        return ResponseEntity.ok().body(appService.getWinner());
+    public ResponseEntity<?> getWinner(){
+        try{
+            return ResponseEntity.ok().body(appService.getWinner());
+        }catch (Exception exc){
+            return ResponseEntity.internalServerError().body("There was an error in the server");
+        }
     }
 
     @GetMapping("/players/{id}/games")
-    public ResponseEntity<ArrayList<Integer[]>> llistatDeJugades(@PathVariable("id") String id){
-        AppUser appUser = appService.getUserById(id);
-        ArrayList<Integer[]> resultat = appUser.getHistoricJugadesFetes();
-        return ResponseEntity.ok().body(resultat);
+    public ResponseEntity<?> llistatDeJugades(@PathVariable("id") String id){
+        try{
+            AppUser appUser = appService.getUserById(id);
+            if (appUser == null){
+                return ResponseEntity.badRequest().body("There is no user with this id");
+            }
+
+            ArrayList<Integer[]> resultat = appUser.getHistoricJugadesFetes();
+            if (resultat == null){
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.ok().body(resultat);
+        }catch (Exception exc){
+
+            return ResponseEntity.internalServerError().body("There was an error in the server");
+        }
     }
 
     @PostMapping("/players/{id}/games")
-    public ResponseEntity<Integer[]> appUserMove(@PathVariable("id") String id){
+    public ResponseEntity<?> appUserMove(@PathVariable("id") String id){
         Integer[] moveResult = null;
-        AppUser appUser = appService.getUserById(id);
-
-        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equalsIgnoreCase(appUser.getUserName())) {
-            moveResult = new Integer[2];
-            moveResult[0] = new Random().nextInt(6) + 1;
-            moveResult[1] = new Random().nextInt(6) + 1;
-
-            appUser.addOneToHistoricDeJugadesFetes(moveResult);
-
-            if (moveResult[0] + moveResult[1] == 7){
-                appUser.afegirPartidaGuanyada();
-                appUser.afegirPartidaFeta();
-            }else if (moveResult[0] + moveResult[1] != 7){
-                appUser.afegirPartidaFeta();
+        try{
+            AppUser appUser = appService.getUserById(id);
+            if (appUser == null){
+                return ResponseEntity.badRequest().body("There is no user with this id");
             }
 
-            appService.modifyUser(appUser);
+
+            if (SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equalsIgnoreCase(appUser.getUserName())) {
+                moveResult = new Integer[2];
+                moveResult[0] = new Random().nextInt(6) + 1;
+                moveResult[1] = new Random().nextInt(6) + 1;
+
+                appUser.addOneToHistoricDeJugadesFetes(moveResult);
+
+                if (moveResult[0] + moveResult[1] == 7) {
+                    appUser.afegirPartidaGuanyada();
+                    appUser.afegirPartidaFeta();
+                } else if (moveResult[0] + moveResult[1] != 7) {
+                    appUser.afegirPartidaFeta();
+                }
+
+                appService.modifyUser(appUser);
 
         }else {
-            moveResult = new Integer[2];
-            moveResult[0] = 0;
-            moveResult[1] = 0;
+            return ResponseEntity.badRequest().body("You are not allowed to make this operation");
         }
         return ResponseEntity.ok().body(moveResult);
+        }catch(Exception exc){
+            return ResponseEntity.internalServerError().body("There has been an internal server error");
+        }
     }
 
 
     @DeleteMapping("/players/{id}/games")
-    public ResponseEntity<AppUser> deleteUserResults(@PathVariable("id") String id){
-        AppUser appUser = appService.getUserById(id);
-        return ResponseEntity.accepted().body(appService.deleteUserMoves(appUser));
+    public ResponseEntity<?> deleteUserResults(@PathVariable("id") String id){
+        try {
+            AppUser appUser = appService.getUserById(id);
+            if (appUser == null) {
+                return ResponseEntity.badRequest().body("There is no user with this id");
+            }
+
+            return ResponseEntity.accepted().body(appService.deleteUserMoves(appUser));
+        }catch(Exception exc){
+            return ResponseEntity.internalServerError().body("There was an internal server error");
+        }
     }
 
 
